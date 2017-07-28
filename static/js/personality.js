299d2c8db2586624a2a842256939f3c3818a5c20
Grutin <m.gruau@gmail.com>
2017-07-28 16:29:07 +0100

    Update personality.js
    Adding consumption preferences categories

--- static/js/personality.js
+++ static/js/personality.js
@@ -193,9 +193,17 @@ function returnLabelList(parent_label) {
                                                          "consumption_preferences_clothes_comfort","consumption_preferences_influence_brand_name","consumption_preferences_influence_utility",
                                                          "consumption_preferences_influence_online_ads","consumption_preferences_influence_social_media","consumption_preferences_influence_family_members",
                                                          "consumption_preferences_spur_of_moment","consumption_preferences_credit_card_payment"]; break;
-        case "Environmental_Concern_Preferences": return ["consumption_preferences_concerned_environment"]; break;
-        case "Entrepreneurship_Preferences": return ["consumption_preferences_start_business"]; break;
-        
+        case "Health_and_Activity_Preferences": return ["consumption_preferences_eat_out","consumption_preferences_gym_membership","consumption_preferences_outdoor"]; break;
+        case "Other": return ["consumption_preferences_concerned_environment,consumption_preferences_start_business"]; break;
+        case "Movie_Preferences": return ["consumption_preferences_movie_romance","consumption_preferences_movie_adventure","consumption_preferences_movie_horror",
+                                          "consumption_preferences_movie_musical","consumption_preferences_movie_historical","consumption_preferences_movie_science_fiction",
+                                          "consumption_preferences_movie_war","consumption_preferences_movie_drama","consumption_preferences_movie_action",
+                                          "consumption_preferences_movie_documentary"]; break;
+        case "Music_Preferences": return ["consumption_preferences_music_rap","consumption_preferences_music_country","consumption_preferences_music_r_b",
+                                          "consumption_preferences_music_hip_hop","consumption_preferences_music_live_event","consumption_preferences_music_playing",
+                                          "consumption_preferences_music_latin","consumption_preferences_music_rock","consumption_preferences_music_classical"]; break;
+        case "Reading_Preferences": return ["consumption_preferences_read_frequency","consumption_preferences_books_entertainment_magazines","consumption_preferences_books_non_fiction",
+                                            "consumption_preferences_books_financial_investing","consumption_preferences_books_autobiographies"]; break;
     }
 }
 
